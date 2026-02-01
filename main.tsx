7c1372bfe4e47c09ae35bfef3149f4a79fa6e9cf
JuanVazquezD <juanbonv@hotmail.com>
2026-02-01 13:50:13 -0600

    Updated main.tsx to import and render the React App component properly.

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Tu archivo CSS para Tailwind
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
